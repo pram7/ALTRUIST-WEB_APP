01bac6e666dbd94ae5140bf2b70f438d05f3654b
Nagasandesh N <50497769+Nagasandesh@users.noreply.github.com>
2021-05-17 16:57:19 +0530

    Add files via upload

--- react/App.js
+++ react/App.js
@@ -1,1 +1,36 @@
+import React from 'react';
+import './App.css';
+import "./website.css";
+import './bootstrap.min.css';
+// import "font-awesome/css/font-awesome.min.css";
 
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar.js';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Chat from './pages/Chats';
+import GetInTouch from './pages/GetInTouch';
+import Ngo from './pages/Ngo';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home}/>
+        <Route path='/Login' exact component={Login}/>
+        <Route path='/Profile' exact component={Profile}/>
+        <Route path='/Chats' exact component={Chat}/>
+        <Route path='/Ngo' exact component={Ngo}/>
+        <Route path='/GetInTouch' exact component={GetInTouch}/>
+      </Switch>
+    </Router>
+    </>
+  );
+}
+
+export default App;
